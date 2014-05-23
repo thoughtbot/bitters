e4c46252077dfd8101a48e2316a4bccc20a88dff
Kyle Fiedler <kyle@kylefiedler.com>
2014-05-23 11:06:27 -0400

    Add test for generating Bitters files

---
+++ spec/bitters_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+require 'sass'
+require 'bourbon'
+
+describe Bitters do
+  after do
+    `rm -rf spec/fixtures/bitters`
+  end
+
+  it 'compiles to valid css' do
+    `bitters install --path spec/fixtures`
+
+    Sass.compile_file('spec/fixtures/application.scss', '/tmp/output.css')
+
+    expect(Pathname('/tmp/output.css')).to exist
+  end
+end
